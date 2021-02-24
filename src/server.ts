ea700a2fc5f2a0b0f5c47f8c41fef6bbeff4df9e
Everton Trindade <everton.trindade@zallpy.com>
2021-02-23 23:56:10 -0300

    Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import * as express from 'express';
+import 'reflect-metadata'
+import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send('Hello World')
-})
-
-app.post("/", (request, response) => {
-
-    return response.json({message: "dados recebidos"})
-
-})
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Servidor iniciado!"))
